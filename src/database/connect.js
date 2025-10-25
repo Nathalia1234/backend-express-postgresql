344dec5a669489753e16c07eecf586846995cee1
Nathalia1234 <nathe557@gmail.com>
2025-10-25 10:58:58 -0300

    -

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,19 +1,22 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
 export const connectDatabase = async () => {
   try {
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    });
-
     await pool.connect();
     console.log("🟢 Conexão com PostgreSQL estabelecida com sucesso!");
   } catch (error) {
     console.error("🔴 Erro ao conectar ao PostgreSQL:", error);
   }
 };
+
+// Exporta o pool para os models usarem
+export { pool };
